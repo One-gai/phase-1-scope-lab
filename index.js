e88c8b324f67551228f7ebaae1e1c6c49af4e89f
Luke <luke.wangai@student.moringaschool.com>
2025-03-16 13:43:18 +0300

    This is all wrong complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+const leastfavouritecustomer= "not bob"
+
+function upperCaseCustomerName(){
+    return  customerName =customerName.toUpperCase()
+     
+}
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer(){
+    return leastfavouritecustomer = "bob"
+}
+ 
+
